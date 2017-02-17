2f63fbcd0da806b7fd239585b0732538287cc16e
yujiwu <cswuyujieapp@126.com>
2017-02-18 06:14:55 +0800

    add boostrap

--- login_verification.php
+++ login_verification.php
@@ -7,10 +7,14 @@ require 'includes/form_handlers/verification_handler.php';
 <html>
 <head>
 	<title>Pop-up Verification</title>
-	<link rel="stylesheet" type="text/css" href="assets/css/register_style.css">
+		<!-- JQuery -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+	<!-- css including boostrap -->
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/register_style.css">
+	<!-- JavaScript -->
+	<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
 	<script type="text/javascript" src="assets/js/register.js"></script>
-
 </head>
 <body>
 
