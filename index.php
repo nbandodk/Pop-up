ace03150c729c69815a9fc2095abac976a19c72f
ninad <ninadbandodkar@gmail.com>
2017-02-17 11:34:32 -0500

    Sprint1ENds

--- index.php
+++ index.php
@@ -1,23 +1,12 @@
 <?php 
-$con = mysqli_connect("localhost", "root", "", "social"); //Connecton var
 
-if(mysqli_connect_errno())
-{
-	echo "Failed to connect: " . mysqli_connect_errno();
-}
-
-$query = mysqli_query($con, "INSERT INTO test VALUES ('1', 'Reece')");
+include("includes/header.php");
 
 ?>
 
 
 
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Pop-up</title>
-</head>
-<body>
-	Hello Noob
+
+	Hello World!
 </body>
 </html>
